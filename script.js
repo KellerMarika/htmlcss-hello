3246db39f0b43389811e1e5c83fa71d011d3c5c5
KellerMarika <marikakeller92@gmail.com>
2022-09-15 23:46:12 +0200

    js

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
-console.log('JS OK')
+/* raccolgo dalla pagina gli elementi */
+
+/* const playButton = document.querySelector('button');
+const ladyfigure = document.querySelector('figure');
+
+
+
+playButton.addEventListener('click',function(){
+
+
+
+    
+}); */
+
+/* var figure = document.getElementById('btn');
+if(onclick){
+/*  */
+/* 
+
+function Change() {
+    document.getElementById('btn').click();
+
+
+
+
+    figure.classList.add("animated");
+}
+
+/* ----------------------------------------- */
+
+
+
+/* 
+document.addEventListener('click', function handleClick(event){
+    console.log('user clicked', event.target)
+    
+    event.target.classList.add('animated');
+}); */
+
+
+/* ------------------------------------ */
+/* const playButton = document.querySelector('button');
+
+const figure = document.getElementById('fig');
+figure.addEventListener()
+
+playButton.addEventListener('click',function handleClick(){
+    figure.classList.add('animated');
+/* });
+     */
+
+/* --------------------------------------- */
+
+
+
+function playButton(){
+    const list = document.getElementById('fig').classList;
+ list.add('animated');
+}
+
+
+
+
+
+
